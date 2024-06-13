30eb2e1e471230aa31dea2dce2f3dff8184dde0f
ZhengWei <HY.ZhengWei@qq.com>
2024-06-13 14:14:28 +0800

    1.0.0
    1. 添加：测试案例

--- src/test/java/org/hy/common/pdf/junit/alipay/JU_Alipay.java
+++ src/test/java/org/hy/common/pdf/junit/alipay/JU_Alipay.java
@@ -35,6 +35,7 @@ public class JU_Alipay
     }
     
     
+    
     /**
      * 生成PDF数据
      * 
@@ -69,6 +70,66 @@ public class JU_Alipay
         v_Texts.add(new PDFText("CNY 500.00"                          ,300F ,374F));
         v_Texts.add(new PDFText("-CNY 50.00"                          ,300F ,356F));
         v_Texts.add(new PDFText("CNY 450.00"                          ,300F ,338F ,"Helvetica-Bold"));
+        
+        return v_Texts;
+    }
+    
+    
+    
+    @Test
+    public void createAlipayRefundPDF()
+    {
+        String v_TemplatePDF = "D:\\WorkSpace\\hy.common.pdf\\src\\test\\java\\org\\hy\\common\\pdf\\junit\\alipay\\Template_Refund.pdf";
+        String v_DataPDF     = "D:\\alipayRefundData.pdf";
+        String v_AlipayPDF   = "D:\\alipayRefund.pdf";
+        
+        if ( PDFHelp.create(v_DataPDF ,makeDatasRefund()) )
+        {
+            PDFHelp.overlay(v_TemplatePDF ,v_DataPDF ,v_AlipayPDF);
+        }
+    }
+    
+    
+    
+    /**
+     * 生成PDF数据（退款的）
+     * 
+     * @author      ZhengWei(HY)
+     * @createDate  2024-06-13
+     * @version     v1.0
+     *
+     * @return
+     */
+    private List<PDFText> makeDatasRefund()
+    {
+        List<PDFText> v_Texts = new ArrayList<PDFText>();
+        
+        // Company
+        v_Texts.add(new PDFText("China Company"                       ,70F ,663F ,12F ,"Helvetica"));
+        v_Texts.add(new PDFText("Xi'an City, Shaanxi Province, China" ,70F  ,644F));
+        v_Texts.add(new PDFText("Wuhan City, Hubei Province, China"   ,70F  ,624F));
+        
+        // Guest Details
+        v_Texts.add(new PDFText("Zheng Wei"                           ,300F ,553F));
+        v_Texts.add(new PDFText("HY.ZhengWei@qq.com"                  ,300F ,534F));
+        
+        // Booking Details
+        v_Texts.add(new PDFText("1234567890"                          ,300F ,499F));
+        v_Texts.add(new PDFText("Correct Hotel"                       ,300F ,481F));
+        v_Texts.add(new PDFText("CANCELED"                            ,300F ,463F));
+        v_Texts.add(new PDFText("2024-06-13"                          ,300F ,444F));
+        v_Texts.add(new PDFText("2024-06-28"                          ,300F ,426F));
+        v_Texts.add(new PDFText("3"                                   ,300F ,408F));
+        v_Texts.add(new PDFText("2024-04-28"                          ,300F ,391F));
+        
+        // Price Details
+        v_Texts.add(new PDFText("CNY 500.00"                          ,300F ,357F));
+        v_Texts.add(new PDFText("-CNY 50.00"                          ,300F ,338F));
+        v_Texts.add(new PDFText("CNY 450.00"                          ,300F ,320F ,"Helvetica-Bold"));
+        
+        // Refund Details
+        v_Texts.add(new PDFText("CNY 300.00"                           ,300F ,284.5F ,"Helvetica"));
+        v_Texts.add(new PDFText("CNY 150.00"                           ,300F ,266F));
         
         return v_Texts;
     }
